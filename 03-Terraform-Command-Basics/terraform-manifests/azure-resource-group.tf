fb836195fea5b87c09d11db2a4d5cb46c588cbb6
GUNNERat19 <swarupkumar1909@gmail.com>
2025-01-29 09:10:01 +0000

    sub_id added

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -12,10 +12,13 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  
+  # Add your subscription ID here
+  subscription_id = "03bcf9e1-bfd0-4535-8262-137108f5b101"
 }
 
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
   location = "eastus"
-  name = "my-demo-rg1"  
+  name     = "my-demo-rg1"  
 }
