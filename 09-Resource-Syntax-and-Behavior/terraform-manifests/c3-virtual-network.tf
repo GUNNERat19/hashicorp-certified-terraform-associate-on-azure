3c0bc89294c50c2fb9cf51c9ccf012f21855e10c
GUNNERat19 <swarupkumar1909@gmail.com>
2025-01-30 09:19:36 +0000

    resource making

--- 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
+++ 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
@@ -1,22 +1,23 @@
-# Resource-2: Create Virtual Network
+# resource to create a virtual network
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet-1"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.myrg.location
+  name = "myvnet-1"
+  location = azurerm_resource_group.myrg.location
+  address_space = ["10.0.0.0/16"]  // [] means list and {} means map in TF
   resource_group_name = azurerm_resource_group.myrg.name
-  tags = {
+   tags = {
     "Name" = "myvnet-1"
     #"Environment" = "Dev"  # Uncomment during Step-10
   }
 }
 
-# Resource-3: Create Subnet
+# to create subnet 
 resource "azurerm_subnet" "mysubnet" {
   name                 = "mysubnet-1"
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
 
 # Resource-4: Create Public IP Address
 resource "azurerm_public_ip" "mypublicip" {
@@ -34,7 +35,8 @@ resource "azurerm_network_interface" "myvmnic" {
   name                = "vmnic"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
-
+  
+  # internal block 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.mysubnet.id
@@ -42,4 +44,3 @@ resource "azurerm_network_interface" "myvmnic" {
     public_ip_address_id = azurerm_public_ip.mypublicip.id 
   }
 }
-
