7f861397ad42a7fb73e2c00e3c4df83ffff33247
GUNNERat19 <swarupkumar1909@gmail.com>
2025-02-03 08:40:39 +0000

    countmeta commit

--- 12-Meta-Argument-for_each-Maps/terraform-manifests/c2-resource-group.tf
+++ 12-Meta-Argument-for_each-Maps/terraform-manifests/c2-resource-group.tf
@@ -1,10 +1,13 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "myrg" {
-  for_each = {
-    dc1apps = "eastus"
-    dc2apps = "eastus2"
-    dc3apps = "westus"
-  }
-  name = "${each.key}-rg"
-  location = each.value 
+  # for_each = {
+  #   dc1apps = "eastus"
+  #   dc2apps = "eastus2"
+  #   dc3apps = "westus"
+  # }
+  # name = "${each.key}-rg"
+  # location = each.value 
+  count = 3
+  name = "myrg-${count.index}"
+  location = "East US"
 }
